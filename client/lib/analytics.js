22f820033cfb766b3350f38fefa0a5cf9e07371b
David Rosson <david@rosson.com.au>
2016-11-22 22:17:42 -0800

    Add Analytics token

---
+++ client/lib/analytics.js
@@ -0,0 +1,9 @@
+if (!window.ga) {
+  window.ga = function() {
+    ga.q.push(arguments);
+  };
+  ga.q = [];
+  ga.l = Number(new Date());
+  ga('create', 'U' + 'A-8714' + '6914-4', 'auto');
+  ga('send', 'pageview');
+}
